76b235755dcf61689c58c9e91ec35fd94869fd86
ZAKI-System <appkari.0@gmail.com>
2021-10-19 23:15:54 +0900

    プロジェクト ファイルを追加します。

---
+++ ShizukuImageConverter/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace ShizukuImageConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.resultPictureBox = new System.Windows.Forms.PictureBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.backWhiteCheckbox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.noDhitherCheckbox = new System.Windows.Forms.CheckBox();
+            this.warnLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.ossLinkLabel = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pathBox
+            // 
+            this.pathBox.AllowDrop = true;
+            this.pathBox.Location = new System.Drawing.Point(13, 13);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(100, 19);
+            this.pathBox.TabIndex = 0;
+            this.pathBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.PathBox_DragDrop);
+            this.pathBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.PathBox_DragEnter);
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(119, 11);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "参照";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(201, 11);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "変換";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(282, 11);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "保存";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // resultPictureBox
+            // 
+            this.resultPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultPictureBox.Location = new System.Drawing.Point(12, 38);
+            this.resultPictureBox.Name = "resultPictureBox";
+            this.resultPictureBox.Size = new System.Drawing.Size(160, 128);
+            this.resultPictureBox.TabIndex = 4;
+            this.resultPictureBox.TabStop = false;
+            // 
+            // backWhiteCheckbox
+            // 
+            this.backWhiteCheckbox.AutoSize = true;
+            this.backWhiteCheckbox.Location = new System.Drawing.Point(178, 40);
+            this.backWhiteCheckbox.Name = "backWhiteCheckbox";
+            this.backWhiteCheckbox.Size = new System.Drawing.Size(111, 16);
+            this.backWhiteCheckbox.TabIndex = 5;
+            this.backWhiteCheckbox.Text = "extentの背景を白";
+            this.backWhiteCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // noDhitherCheckbox
+            // 
+            this.noDhitherCheckbox.AutoSize = true;
+            this.noDhitherCheckbox.Location = new System.Drawing.Point(178, 62);
+            this.noDhitherCheckbox.Name = "noDhitherCheckbox";
+            this.noDhitherCheckbox.Size = new System.Drawing.Size(70, 16);
+            this.noDhitherCheckbox.TabIndex = 6;
+            this.noDhitherCheckbox.Text = "ディザなし";
+            this.noDhitherCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 72);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "1 参照またはパスを直接入力\r\n2 変換\r\nオプション変更後は再度変換\r\n3 txtで保存\r\n4 Shizukuの/img/ にtxtを入れて\r\n  list.t" +
+    "xtにファイル名を追記";
+            // 
+            // warnLinkLabel
+            // 
+            this.warnLinkLabel.AutoSize = true;
+            this.warnLinkLabel.Location = new System.Drawing.Point(310, 190);
+            this.warnLinkLabel.Name = "warnLinkLabel";
+            this.warnLinkLabel.Size = new System.Drawing.Size(29, 12);
+            this.warnLinkLabel.TabIndex = 8;
+            this.warnLinkLabel.TabStop = true;
+            this.warnLinkLabel.Text = "注意";
+            this.warnLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.WarnLinkLabel_LinkClicked);
+            // 
+            // ossLinkLabel
+            // 
+            this.ossLinkLabel.AutoSize = true;
+            this.ossLinkLabel.Location = new System.Drawing.Point(345, 190);
+            this.ossLinkLabel.Name = "ossLinkLabel";
+            this.ossLinkLabel.Size = new System.Drawing.Size(27, 12);
+            this.ossLinkLabel.TabIndex = 9;
+            this.ossLinkLabel.TabStop = true;
+            this.ossLinkLabel.Text = "OSS";
+            this.ossLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OssLinkLabel_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.ossLinkLabel);
+            this.Controls.Add(this.warnLinkLabel);
+            this.Controls.Add(this.noDhitherCheckbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.backWhiteCheckbox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.resultPictureBox);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.pathBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Shizuku(CT-3) Image Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.PictureBox resultPictureBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.CheckBox backWhiteCheckbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox noDhitherCheckbox;
+        private System.Windows.Forms.LinkLabel warnLinkLabel;
+        private System.Windows.Forms.LinkLabel ossLinkLabel;
+    }
+}
+
---
+++ ShizukuImageConverter/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageMagick;
+#if Q8
+using QuantumType = System.Byte;
+#elif Q16
+using QuantumType = System.UInt16;
+#elif Q16HDRI
+using QuantumType = System.Single;
+#else
+#error Not implemented!
+#endif
+
+namespace ShizukuImageConverter
+{
+    public partial class Form1 : Form
+    {
+        private const int SzkW = 160;
+        private const int SzkH = 128;
+
+        private string loadedFileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>表示中の画像をShizuku用txt形式で保存</summary>
+        private void Save()
+        {
+            if (resultPictureBox.Image == null)
+            {
+                MessageBox.Show("画像がロードされていません", "error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string savePath = Path.Combine(Path.GetDirectoryName(loadedFileName), Path.GetFileNameWithoutExtension(loadedFileName) + "-szkc.txt");
+            if (MessageBox.Show(savePath+"\nに保存しますか？(同名のファイルは上書きされます)", "保存確認", MessageBoxButtons.YesNo, MessageBoxIcon.Information) != DialogResult.Yes)
+            {
+                return;
+            }
+
+            // PixelScan開始
+            using (var bitmap = new Bitmap(resultPictureBox.Image))
+            using (var outputStream = new MemoryStream())
+            using (var sw = new StreamWriter(outputStream))
+            using (var fs = new FileStream(savePath, FileMode.Create))
+            {
+                // 縦
+                for (int y = 0; y < SzkH; y++)
+                {
+                    // 横
+                    for (int x = 0; x < SzkW; x++)
+                    {
+                        // 画像サイズ範囲内か
+                        if (x < bitmap.Width && y < bitmap.Height)
+                        {
+                            Color color = bitmap.GetPixel(x, y);
+                            if (color.R == 255 && color.G == 0 && color.B == 0)
+                            {
+                                sw.Write("0");
+                            }
+                            else if (color.R == 0 && color.G == 0 && color.B == 255)
+                            {
+                                sw.Write("1");
+                            }
+                            else if (color.R == 0 && color.G == 128 && color.B == 0)
+                            {
+                                sw.Write("2");
+                            }
+                            else if (color.R == 0 && color.G == 255 && color.B == 255)
+                            {
+                                sw.Write("3");
+                            }
+                            else if (color.R == 0 && color.G == 0 && color.B == 0)
+                            {
+                                sw.Write("4");
+                            }
+                            else if (color.R == 255 && color.G == 255 && color.B == 255)
+                            {
+                                sw.Write("5");
+                            }
+                            else if (color.R == 128 && color.G == 0 && color.B == 128)
+                            {
+                                sw.Write("6");
+                            }
+                            else if (color.R == 255 && color.G == 165 && color.B == 0)
+                            {
+                                sw.Write("7");
+                            }
+                            else if (color.R == 128 && color.G == 128 && color.B == 128)
+                            {
+                                sw.Write("8");
+                            }
+                            else if (color.R == 255 && color.G == 255 && color.B == 0)
+                            {
+                                sw.Write("9");
+                            }
+                            else if (color.R == 144 && color.G == 238 && color.B == 144)
+                            {
+                                sw.Write("10");
+                            }
+                            else if (color.R == 240 && color.G == 128 && color.B == 128)
+                            {
+                                sw.Write("11");
+                            }
+                            else if (color.R == 255 && color.G == 255 && color.B == 224)
+                            {
+                                sw.Write("12");
+                            }
+                            else if (color.R == 173 && color.G == 216 && color.B == 230)
+                            {
+                                sw.Write("13");
+                            }
+                            else if (color.R == 255 && color.G == 182 && color.B == 193)
+                            //else if (color.R == 248 && color.G == 164 && color.B == 248)
+                            {
+                                sw.Write("14");
+                            }
+                            else if (color.R == 169 && color.G == 169 && color.B == 169)
+                            {
+                                sw.Write("15");
+                            }
+                            else
+                            {
+                                sw.Write("16");
+                            }
+                        }
+                        else
+                        {
+                            sw.Write("16");
+                        }
+                        // 1pixel書き終わったら区切り
+                        sw.Write(",");
+                    }
+                    // 横のpixelを全て書き終えたら改行
+                    sw.WriteLine("");
+                }
+                // 反映、保存
+                sw.Flush();
+                outputStream.Seek(0, SeekOrigin.Begin);
+                outputStream.CopyTo(fs);
+            }
+            MessageBox.Show(savePath+"\n入力と同じディレクトリに保存しました。\n使用時はShizukuの/img/ に入れ、list.txtにファイル名を追記してください。");
+        }
+
+        /// <summary>BitmapをShizuku向けに変換</summary>
+        /// <param name="bitmap">入力画像Bitmap</param>
+        /// <returns>Shizuku向けに変換された画像Bitmap</returns>
+        private Bitmap Convert(Bitmap bitmap)
+        {
+            using (var inputStream = new MemoryStream())
+            {
+                // streamに展開
+                bitmap.Save(inputStream, System.Drawing.Imaging.ImageFormat.Png);
+                inputStream.Seek(0, SeekOrigin.Begin);
+                using (var workImg = new MagickImage(inputStream))
+                {
+                    // サイズ変更と減色
+                    using (IMagickImage<QuantumType> colors = CreatePalleteImage())
+                    {
+                        workImg.Resize(SzkW, SzkH);
+                        workImg.Extent(SzkW, SzkH, Gravity.Center, backWhiteCheckbox.Checked ? MagickColors.White : MagickColors.Transparent);
+                        if (noDhitherCheckbox.Checked)
+                        {
+                            workImg.Map(colors, new QuantizeSettings() { DitherMethod = DitherMethod.No });
+                        }
+                        else
+                        {
+                            workImg.Map(colors);
+                        }
+                    }
+                    // 画像書き込みと生成
+                    using (var outputStream = new MemoryStream())
+                    {
+                        workImg.Format = MagickFormat.Png;
+                        workImg.Write(outputStream);
+                        outputStream.Position = 0;
+                        return new Bitmap(outputStream);
+                    }
+                }
+            }
+        }
+
+        /// <summary>Shizuku減色用色画像生成</summary>
+        /// <returns>減色用色画像</returns>
+        private IMagickImage<QuantumType> CreatePalleteImage()
+        {
+            using (var images = new MagickImageCollection())
+            {
+                images.Add(new MagickImage(MagickColors.Red, 1, 1));
+                images.Add(new MagickImage(MagickColors.Blue, 1, 1));
+                images.Add(new MagickImage(MagickColors.Green, 1, 1));
+                images.Add(new MagickImage(MagickColors.Cyan, 1, 1));
+                images.Add(new MagickImage(MagickColors.Black, 1, 1));
+                images.Add(new MagickImage(MagickColors.White, 1, 1));
+                images.Add(new MagickImage(MagickColors.Purple, 1, 1));
+                images.Add(new MagickImage(MagickColors.Orange, 1, 1));
+                images.Add(new MagickImage(MagickColors.Gray, 1, 1));
+                images.Add(new MagickImage(MagickColors.Yellow, 1, 1));
+                images.Add(new MagickImage(MagickColors.LightGreen, 1, 1));
+                images.Add(new MagickImage(MagickColors.LightCoral, 1, 1));//
+                images.Add(new MagickImage(MagickColors.LightYellow, 1, 1));
+                images.Add(new MagickImage(MagickColors.LightBlue, 1, 1));
+                images.Add(new MagickImage(MagickColors.LightPink, 1, 1));//
+                //images.Add(new MagickImage(new MagickColor("#F8A4F8FF"), 1, 1));//?
+                images.Add(new MagickImage(MagickColors.DarkGray, 1, 1));
+
+                return images.AppendHorizontally();
+            }
+        }
+
+        /// <summary>D&amp;D開始時</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PathBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>D&amp;D処理</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PathBox_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (var file in files)
+            {
+                pathBox.Text = file;
+                // 最初のファイルのみ
+                break;
+            }
+        }
+
+        /// <summary>参照ボタン</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            using (var fileDialog = new OpenFileDialog())
+            {
+                fileDialog.Filter = "PNG画像ファイル (*.png)|*.png|JPG画像ファイル (*.jpg)|*.jpg";
+
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // 読み込み
+                    pathBox.Text = fileDialog.FileName;
+                }
+            }
+        }
+
+        /// <summary>変換ボタン</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            pathBox.Text = pathBox.Text.Replace("\"", "");
+            if (File.Exists(pathBox.Text))
+            {
+                loadedFileName = pathBox.Text;
+                try
+                {
+                    using (var bitmap = new Bitmap(pathBox.Text))
+                    {
+                        resultPictureBox.Image = Convert(bitmap);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("ファイルが存在しません", "error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        /// <summary>保存ボタン</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Save();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>注意事項クリック</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WarnLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("このアプリケーションは個人開発です。バグが存在する可能性があります。\n保証はありません。", "注意事項", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        /// <summary>OSS一覧</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OssLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Magick.NET", "使用OSS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
